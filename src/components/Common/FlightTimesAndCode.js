f28f47e63a3668ac9bbd4b9f1e3edf0f187a0f4c
ryananthonydiaz <email@ryananthonydiaz.com>
2019-09-30 13:43:44 -0700

    IconTimePrice

---
+++ src/components/Common/FlightTimesAndCode.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FlightTimesAndCode = () => {
+	return <div />;
+};
+
+export default FlightTimesAndCode;
--- src/shared/app-constants.js
+++ src/shared/app-constants.js
@@ -1,4 +1,24 @@
+import Delta from '../images/delta.svg';
+import SouthWest from '../images/southwest.svg';
+import United from '../images/united-airlines.svg';
+import American from '../images/american-airlines.svg';
+import AlaskaAirlines from '../images/alaska-airlines.svg';
+import AirCanada from '../images/air-canada.svg';
+
 export const trip = {
-  roundTrip: 'Round trip',
-  oneWay: 'One way'
+	roundTrip: 'Round trip',
+	oneWay: 'One way',
 };
+
+export const stops = { nonStop: 'Nonstop' };
+
+export const AirlineToIcon = {
+	'United Airlines': United,
+	'Air Canada': AirCanada,
+	'Alaska Airlines': AlaskaAirlines,
+	'American Airlines': American,
+	'Delta Airlines': Delta,
+	'Southwest Airlines': SouthWest,
+};
+
+export const testFlights = {};
